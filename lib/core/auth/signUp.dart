1c319604548012f4434c8dcbefb5ee9c9f92abb6
techwizard-mobile-ai <dylan1406lewis@gmail.com>
2024-12-11 01:08:08 -0500

    second update

--- lib/core/auth/signUp.dart
+++ lib/core/auth/signUp.dart
@@ -3,6 +3,7 @@
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:faith_mobile/utils/validator/signup.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 Future<String?> signUpUser(String? email, String? password) async {
   bool errorMessage = signUpValidator(email, password);
@@ -12,10 +13,15 @@ Future<String?> signUpUser(String? email, String? password) async {
   }
   final FirebaseAuth _auth = FirebaseAuth.instance;
   try {
-    UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
-        email: email!, password: password!);
-    userCredential.user?.sendEmailVerification();
-    print("User signed Up: ${userCredential.user?.uid}");
+    // UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+    //     email: email!, password: password!);
+    final Uri emailTemp = Uri(
+        scheme: 'mailto',
+        path: 'dylan1406lewis@gmail.com',
+        query:
+            'subject=Email Verification&body=Please verify your email address by clicking on the link below:');
+    await launchUrl(emailTemp, mode: LaunchMode.externalApplication);
+    // print("User signed Up: ${userCredential.user?.uid}");
     return null;
   } on FirebaseAuthException catch (e) {
     print(e.message);
